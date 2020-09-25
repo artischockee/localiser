afb6a41b59a963d03ac6ac0f86aae354f62063dd
Artem Piskarev <artyeug@gmail.com>
2020-09-26 00:40:26 +0700

    types: Add types.ts

---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface LocaleConfig {
+  locale: string;
+  l(locKey: string, params?: Record<string, any> | null, locale?: string): string;
+}
+
+export interface LocaleContextModel {
+  localeResources: Record<string, Record<string, string>>;
+  fallbackLocale?: string;
+}
