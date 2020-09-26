7030b4ce1300ae6fd53dc642ffcad72731b8841c
Artem Piskarev <artyeug@gmail.com>
2020-09-26 14:29:03 +0700

    feat: Implement instance.ts

---
+++ src/instance.ts
@@ -0,0 +1,9 @@
+import LocaliserInstance from './LocaliserInstance';
+
+interface _LocaliserInstance {
+  instance: LocaliserInstance | null;
+}
+
+export const localiser: _LocaliserInstance = {
+  instance: null,
+};
