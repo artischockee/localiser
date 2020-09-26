7c8fee4869ceba3bd58ec39f996beb68fd47c5e5
Artem Piskarev <artyeug@gmail.com>
2020-09-26 14:42:12 +0700

    feat: Add useEffect which changes locale of the Localiser instance dynamically

--- src/LocaleProvider.tsx
+++ src/LocaleProvider.tsx
@@ -1,6 +1,7 @@
-import React from "react";
-import LocaleContext from "./LocaleContext";
-import LocaliserInstance from "./LocaliserInstance";
+import React, { useEffect } from 'react';
+import LocaleContext from './LocaleContext';
+import LocaliserInstance from './LocaliserInstance';
+import { localiser } from './instance';
 
 interface Props {
   config: LocaliserInstance;
@@ -9,12 +10,20 @@ interface Props {
 }
 
 export default function LocaleProvider(props: Props) {
+  useEffect(() => {
+    if (localiser.instance instanceof LocaliserInstance) {
+      localiser.instance.locale = props.locale;
+    }
+  }, [props.locale]);
+
   return (
-    <LocaleContext.Provider value={{
-      locale: props.locale,
-      l: props.config.l,
-    }}>
+    <LocaleContext.Provider
+      value={{
+        locale: props.locale,
+        l: props.config.l,
+      }}
+    >
       {props.children}
     </LocaleContext.Provider>
-  )
+  );
 }
